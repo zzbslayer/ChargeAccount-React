7e3d728ff567311d95e26ad7680979689ee842cf
zzbslayer <zzbslayer@sjtu.edu.cn>
2018-05-15 20:43:52 +0800

    Added travis file and homepage

--- src/ChargeAccount/ChargeAccount.js
+++ src/ChargeAccount/ChargeAccount.js
@@ -6,12 +6,12 @@ class Record extends React.Component{
       if(s==='')
          return;
       n = n > 0 && n <= 20 ? n : 2;   
-      s = parseFloat((s + "").replace(/[^\d\.-]/g, "")).toFixed(n) + "";   
+      s = parseFloat((s + "").replace(/[^\d-]/g, "")).toFixed(n) + "";   
       var l = s.split(".")[0].split("").reverse(),   
       r = s.split(".")[1];   
       var t = "";   
       for(let i = 0; i < l.length; i ++ ) {   
-          t += l[i] + ((i + 1) % 3 == 0 && (i + 1) != l.length ? "," : "");   
+          t += l[i] + ((i + 1) % 3 === 0 && (i + 1) !== l.length ? "," : "");   
       }   
       return t.split("").reverse().join("") + "." + r;   
   } 
